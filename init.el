629decf433b6a64a684d8dbdf49e4800a9a51d43
Ian Gagorik <ian.gagorik@gmail.com>
2023-07-20 19:58:11 -0700

    Adds emacs config to help keep files clean

--- init.el
+++ init.el
@@ -29,3 +29,15 @@
 ;; Revert buffers when the underlying files or buffers have changed
 (global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-buffers t)
+
+;; Keep files clean
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+(setq auto-save-list-file-prefix (expand-file-name "tmp/autosaves/sessiosns/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
+(setq projectile-know-projects-file (expand-file-name "tmp/projectile-bookmarks.eld" user-emacs-directory)
+      lsp-session-file (expand-file-name "tmp/.lsp-session-v1" user-emacs-directory))
+
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs"))
