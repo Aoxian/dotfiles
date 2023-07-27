56606dbedb11f601c36c7a1c3f31eb35fb9a4e8c
Ian Gagorik <ian.gagorik@gmail.com>
2023-07-27 13:03:54 -0700

    Adds package mgmt to emacs

--- init.el
+++ init.el
@@ -30,6 +30,26 @@
 (setq global-auto-revert-non-file-buffers t)
 
 
+;; Package Mgmt
+
+;;; Initialize package sources
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
 ;; Modus Theme config
 
 ;;; Use latest version of modus themes
