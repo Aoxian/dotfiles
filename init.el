6d618b9eaea951ba74066a505ce9b168009d4ad4
Ian Gagorik <ian.gagorik@gmail.com>
2023-07-28 15:33:32 -0700

    Emacs uses extensible vi layer

--- init.el
+++ init.el
@@ -53,6 +53,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+
 ;;; Use Ivy with counsel for completions
 (use-package ivy
   :diminish
@@ -78,6 +79,31 @@
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
+
+;;; Extensible VI Layer
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  (evil-mode t)
+  :config
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+;;;; Use visual line motions even outside of visual-line-mode-buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 
 ;;; Doom Modeline & Themes
 
