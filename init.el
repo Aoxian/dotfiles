a9d50a808deb72d89ee54d2ad4395c3c7f1c6729
Ian Gagorik <ian.gagorik@gmail.com>
2023-08-04 17:44:40 -0700

    Adds OrgMode configs form Emacs

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; UI Settings
 
+;;; Default Font Size control
+(defvar aoxian/default-font-size 180)
+
 ;;; Turn off some uneeded UI elements
 (menu-bar-mode -1)                      ; Don't show the menu bar
 (tool-bar-mode -1)                      ; Don't show the tool bar
@@ -32,6 +35,17 @@
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+;; Font Configuration
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height aoxian/default-font-size)
+
+;;;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 260)
+
+;;;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 295 :weight 'regular)
 
 
 ;; Package Mgmt
@@ -167,6 +181,54 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 (use-package forge)
+
+;;; OrgMode
+(defun aoxian/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun aoxian/org-font-setup ()
+;;;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+;;;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . aoxian/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (aoxian/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;;;; visual fill
+(defun aoxian/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . aoxian/org-mode-visual-fill))
 
 ;;; Doom Modeline & Themes
 
